0d53a4978a63a5095da5e0fc61cfcfd03d08a434
Administrator <Administrator@W2K8R2>
2017-07-18 12:31:27 +0900

    Update for DE1

--- Hello/Hello.c
+++ Hello/Hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-__declspec(dllexport) void Hello(int arry) {
+__declspec(dllexport) void Hello() {
 	printf("共有ライブラリ｢Hello｣から出力\n");
 
 	char num[] = "1234567890";
+	int arry = 11;
 	printf("num[%i]=%i\n", arry, num[arry]);
 
 }
