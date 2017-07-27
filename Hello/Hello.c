ec4167c60fb58c45a2a27794eb204a364fb2ceb4
iwahi01 <iwahi01@ca.com>
2017-07-27 11:19:17 +0900

    Update DE1

--- Hello/Hello.c
+++ Hello/Hello.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-__declspec(dllexport) void Hello() {
+__declspec(dllexport) int Hello(int arry) {
 	printf("共有ライブラリ｢Hello｣から出力\n");
-//Update for DE1
+	//Update DE1
 	char num[] = "1234567890";
-	int arry = 11;
 	printf("num[%i]=%i\n", arry, num[arry]);
-
+	return num[arry];
 }
--- Hello_C/Hello_C.c
+++ Hello_C/Hello_C.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-__declspec(dllimport) void Hello();
+__declspec(dllimport) int Hello(int);
 
 void main() {
 	printf("呼び出し元｢Hello_C｣から出力\n");
-	Hello();
+	int val = Hello(99);
+	printf("呼び出し先｢Hello｣からの戻り値=%i\n", val);
 }
