f4c7be41e9a68410a25317d6a837798542972251
iwahi01 <iwahi01@ca.com>
2017-09-06 17:09:44 +0900

    Update for DE1

--- Hello/Hello.c
+++ Hello/Hello.c
@@ -2,7 +2,7 @@
 
 __declspec(dllexport) int Hello(int arry) {
 	printf("共有ライブラリ｢Hello｣から出力\n");
-	//Update for DE1
+	//Update x for DE1
 	char num[] = "1234567890";
 	printf("num[%i]=%i\n", arry, num[arry]);
 	return num[arry];
